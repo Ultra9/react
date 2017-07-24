b191a44ecf4873f4cad52f7952006bc863604d83
Irina Sankova <radioisolation@gmail.com>
2017-07-24 21:51:46 +0700

    add index.html

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const title = React.createElement(
     'h1',
     {id: 'title'},
-    'Hello React'
+    'Tits and Dragons'
 )
-ReactDOM.render{
+ReactDOM.render(
     title,
     document.getElementById('container')
-}
+)
